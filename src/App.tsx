e1b2612616fb20a725afa0faa33042c419d4ee26
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-08-05 23:55:59 +0100

    testing api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      Word Hunter
-    </div>
-  );
+  const [meanings, setmeanings] = useState([])
+  const [word, setword] = useState("")
+
+  const fetchDictionaryApi = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/play"
+      )
+      setmeanings(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchDictionaryApi()
+  }, [])
+
+  console.log(meanings)
+  return <div className="App">Word Hunter</div>
 }
 
-export default App;
+export default App
